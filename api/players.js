c92b23bf059789e393346699cc28134a1fede5c4
PureMortic <eternalbeastgohan@gmail.com>
2025-12-30 23:10:34 +0500

    Add files via upload

--- api/players.js
+++ api/players.js
@@ -1,1 +1,17 @@
+let players = [];
 
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    
+    if (req.method === 'GET') {
+        return res.json(players);
+    }
+    
+    if (req.method === 'POST') {
+        players = req.body;
+        return res.json({ success: true });
+    }
+    
+    res.status(405).json({ error: 'Method not allowed' });
+}
